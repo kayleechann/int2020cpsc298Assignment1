864fbeeca67b38b5c4230b6cb19e526b13b30313
root <kaychan@chapman.edu>
2020-01-29 21:12:55 +0000

    added comments to fire capacity problem

--- ProblemFirecapacity.cpp
+++ ProblemFirecapacity.cpp
@@ -2,23 +2,25 @@
 using namespace std;
 
 int main(int argc, char **argv){
+  //declare variables
   int maxRoomCapacity;
   int numPeople;
   int remainingPeople;
 
   cout << "How many people are in the room? ";
-  cin >> numPeople;
+  cin >> numPeople; //get user inout
   cout << "What is the maximum room capacity? ";
-  cin >> maxRoomCapacity;
+  cin >> maxRoomCapacity; //get max room capacity
 
+  //if the number of people is less than or equal to maxRoomCapacity
   if (numPeople <= maxRoomCapacity){
-    remainingPeople = maxRoomCapacity - numPeople;
+    remainingPeople = maxRoomCapacity - numPeople; //meeting is legal
     cout << "meeting is legal! " << remainingPeople << " additional people may legally attend the meeting." << endl;
   }
-  else{
+  else{ //if number of people exceeds maxRoomCapacity
     remainingPeople = numPeople - maxRoomCapacity;
     cout << "meeting is not legal due to fire regulations. " << remainingPeople << " or more people must be excluded to meet fire regualtions. " << endl;
-  }
+  } //calculate people who must leave
 
   return 0;
 }
