80b1aac4b278b0818183639a74c2b304ccb378ca
root <kaychan@chapman.edu>
2020-01-29 21:14:47 +0000

    added comments to inflation problem

--- ProblemInflation.cpp
+++ ProblemInflation.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-
+  //declaring variables
   float cost;
   int yearsFromNow;
   double rate;
 
   cout << "What is the the current cost of the item? ";
-  cin >> cost;
+  cin >> cost; //get current cost of item from user
   cout << "How many years from now will the item be purchased? ";
-  cin >> yearsFromNow;
+  cin >> yearsFromNow; //get years from now from user
   cout << "What is the rate of inflation? (enter as percentage) ";
-  cin >> rate;
+  cin >> rate; //get inflation rate
 
-  rate = rate / 100;
+  rate = rate / 100; //find rate in percent
+
   for(int i = 0; i < yearsFromNow; ++i)
   {
-    cost = cost * (1+rate);
+    cost = cost * (1+rate); //calculate the expected cost
   }
 
-  cout << "The expected cost of the item " << yearsFromNow <<" years from now is: $" << cost << endl;
+  cout << "The expected cost of the item " << yearsFromNow <<" years from now is: $" << cost << endl; //print to screen the expected cost
 
 
   return 0;
