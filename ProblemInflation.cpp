3bd4b07e71b4ff2d70485c303c02cc582afc92ea
root <kaychan@chapman.edu>
2020-01-29 20:58:45 +0000

    finished last problem

--- ProblemInflation.cpp
+++ ProblemInflation.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
+
+  float cost;
+  int yearsFromNow;
+  double rate;
+
+  cout << "What is the the current cost of the item? ";
+  cin >> cost;
+  cout << "How many years from now will the item be purchased? ";
+  cin >> yearsFromNow;
+  cout << "What is the rate of inflation? (enter as percentage) ";
+  cin >> rate;
+
+  rate = rate / 100;
+  for(int i = 0; i < yearsFromNow; ++i)
+  {
+    cost = cost * (1+rate);
+  }
+
+  cout << "The expected cost of the item " << yearsFromNow << " is: $" << cost << endl;
 
 
   return 0;
